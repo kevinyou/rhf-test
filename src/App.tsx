728bbb02cb260f94163f9677b8c4073afdab1391
Kevin You <dyou@opengov.com>
2024-09-24 18:57:07 -0500

    Use Controller for MUI RadioGroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from '@mui/material';
+import { FormControl, FormControlLabel, FormHelperText, FormLabel, Radio, RadioGroup, TextField } from '@mui/material';
 import { useEffect } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 // import './App.css'
 
 type Foo = {
   firstName: string;
   lastName: string;
   smoking: 'yes' | 'no';
+  smokingx: 'yes' | 'no';
 }
 
 function App() {
   const {
+    control,
     register,
     handleSubmit,
     formState: { errors },
     watch,
   } = useForm<Foo>({
     defaultValues: {
-      firstName: 'test',
-      smoking: 'yes',
+      firstName: 'Bob',
     }
   });
 
   const onSubmit: SubmitHandler<Foo> = (data) => console.log(data);
 
-   // Callback version of watch.  It's your responsibility to unsubscribe when done.
    useEffect(() => {
     const subscription = watch((value, { name, type }) =>
       console.log(value, name, type)
@@ -41,11 +41,18 @@ function App() {
 
           <FormControl>
             <FormLabel>Do you Smoke?</FormLabel>
-            <RadioGroup>
-              <FormControlLabel control={<Radio />} value='yes' label='Yes' {...register('smoking')} />
-              <FormControlLabel control={<Radio />} value='no' label='No' {...register('smoking')} />
-              {errors.smoking && <span>{errors.smoking.message}</span>}
-            </RadioGroup>
+            <Controller
+              name="smoking"
+              control={control}
+              rules={{ required: 'This field is required' }}
+              render={({ field }) => (
+                <RadioGroup {...field}>
+                  <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                  <FormControlLabel value="no" control={<Radio />} label="No" />
+                </RadioGroup>
+              )}
+            />
+            {errors.smoking && <FormHelperText>{errors.smoking.message}</FormHelperText>}
           </FormControl>
 
           <input type="submit" onClick={handleSubmit(onSubmit)}/>
