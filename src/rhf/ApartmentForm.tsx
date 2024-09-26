0e3620cfbe7e0eac87afd505dd65c52f044feb7c
Kevin You <dyou@opengov.com>
2024-09-26 16:22:40 -0500

    Swap from useFormik hook to Formik and useFormikContext, in the process requiring some wrapped components

--- src/rhf/ApartmentForm.tsx
+++ src/rhf/ApartmentForm.tsx
@@ -1,42 +1,57 @@
 import { Button, FormControl, FormControlLabel, FormHelperText, FormLabel, Radio, RadioGroup, TextField } from '@mui/material';
-import { getIn, useFormik } from 'formik';
+import { Form, Formik, getIn, useFormikContext } from 'formik';
 import { ApartmentApplication } from '../apartmentForm';
 // import './App.css'
+
+const NameFields = () => {
+  const formik = useFormikContext<ApartmentApplication>();
+  return (
+    <>
+      <TextField label='First Name' {...formik.getFieldProps('firstName')}/>
+      <TextField label='Last Name' {...formik.getFieldProps('lastName')} />
+    </>
+  )
+}
+
+const MedicalProfileField = () => {
+  const formik = useFormikContext<ApartmentApplication>();
+
+  return (
+    <FormControl>
+      <FormLabel>Do you Smoke?</FormLabel>
+      <RadioGroup {...formik.getFieldProps('medicalProfile.smoking')}>
+        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="no" control={<Radio />} label="No" />
+      </RadioGroup>
+      { getIn(formik.errors, 'medicalProfile.smoking') && <FormHelperText>{getIn(formik.errors, 'medicalProfile.smoking')}</FormHelperText>}
+    </FormControl>
+  )
+}
 
 export const ApartmentForm = () => {
   const onSubmit = (data: any) => console.log(data);
 
-  const formik = useFormik<ApartmentApplication>({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      medicalProfile: {
-        smoking: null,
-      },
-      pets: [],
-    },
-    onSubmit: onSubmit,
-  })
-
   return (
-    <>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        medicalProfile: {
+          smoking: null,
+        },
+        pets: [],
+      }}
+      onSubmit={onSubmit}
+    >
       <div>
-        <form onSubmit={formik.handleSubmit}>
-          <TextField label='First Name' {...formik.getFieldProps('firstName')}/>
-          <TextField label='Last Name' {...formik.getFieldProps('lastName')} />
+        <Form>
+          <NameFields />
 
-          <FormControl>
-            <FormLabel>Do you Smoke?</FormLabel>
-            <RadioGroup {...formik.getFieldProps('medicalProfile.smoking')}>
-              <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-              <FormControlLabel value="no" control={<Radio />} label="No" />
-            </RadioGroup>
-            { getIn(formik.errors, 'medicalProfile.smoking') && <FormHelperText>{getIn(formik.errors, 'medicalProfile.smoking')}</FormHelperText>}
-          </FormControl>
+          <MedicalProfileField/>
 
           <Button type='submit'>Submit</Button>
-        </form>
+        </Form>
       </div>
-    </>
+    </Formik>
   )
 }
