edfbbb7ea9d6e2370675d4c40a5ab165926e3117
Kevin You <dyou@opengov.com>
2024-09-27 15:02:57 -0500

    Show example with a conditional field

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { AutoForm } from 'uniforms-mui';
-import { bridge as schema } from './schema'
+import { getBridge } from './schema'
+import { useMemo, useState } from 'react';
+import { Button } from '@mui/material';
 
 function App() {
+  const [showPets, setShowPets] = useState(false);
+  const bridge = useMemo(() => getBridge({ showPets }), [showPets]);
   return (
-    <AutoForm schema={schema} onSubmit={console.log}>
-    </AutoForm>
+    <div>
+      <Button onClick={() => setShowPets(!showPets)}>Toggle Pet Manager</Button>
+      <AutoForm schema={bridge} onSubmit={console.log}>
+      </AutoForm>
+    </div>
   )
 }
 
